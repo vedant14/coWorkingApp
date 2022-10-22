df17bd0b26b39e16b3bf5b5f1456587f455059b5
Vedant Lohbare <vedant.lohbare@gmail.com>
2022-10-23 00:24:20 +0530

    Brand page'

--- components/ShowAllBrands/index.js
+++ components/ShowAllBrands/index.js
@@ -14,13 +14,41 @@ export function ShowAllBrands() {
     <div>
       <div className="h1">All your brands</div>
       {brandData && (
-        <div>
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+        >
           {brandData.map((item, i) => (
-            <div key={i}>
-              <Link href={`/brands/${item.id}`}>{item.brandName}</Link>
-            </div>
+            <li
+              key={i}
+              className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200"
+            >
+              <div className="w-full flex items-center justify-between p-6 space-x-6">
+                <div className="flex-1 truncate">
+                  <div className="flex items-center space-x-3">
+                    <h3 className="text-gray-900 text-sm font-medium truncate">
+                      {item.brandName}
+                    </h3>
+                    <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                      LIVE
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="-mt-px flex divide-x divide-gray-200">
+                  <div className="-ml-px w-0 flex-1 flex">
+                    <Link href={`/brands/${item.id}`} passHref>
+                      <div className="relative w-0 cursor-pointer flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
+                        <span className="ml-3">View Brand</span>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </li>
           ))}
-        </div>
+        </ul>
       )}
     </div>
   );
