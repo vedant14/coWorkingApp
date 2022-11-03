2f0ba982c1df5e142b2f65b03f40a65f65484b77
Vedant Lohbare <vedant.lohbare@gmail.com>
2022-11-04 00:10:16 +0530

    Refactoring

--- components/CheckoutForm/index.js
+++ components/CheckoutForm/index.js
@@ -1,29 +1,12 @@
 import { useState } from "react";
-import axios from "axios";
-import { createOrder } from "../../utils/supabasePostRequests";
 import { BrandButton } from "../atoms/brandButton";
 import { DropDown } from "./dropDown";
 import { InputText } from "./inputText";
 import { PageLoaderSpinner } from "./pageLoader";
 import { SuccessModal } from "./successModal";
+import { makePayment } from "../../utils/paymentCalls";
+import passArray from "../../data/passArray.json";
 
-const passArray = [
-  {
-    id: "1",
-    name: "Daily Pass",
-    metric: "days",
-  },
-  {
-    id: "2",
-    name: "Weekly Pass",
-    metric: "weeks",
-  },
-  {
-    id: "3",
-    name: "Monthly Pass",
-    metric: "months",
-  },
-];
 var quanityArray = [];
 
 for (let i = 1; i < 30; i++) {
@@ -43,76 +26,11 @@ export function CheckoutForm({ bookingData }) {
   var label = `No. of ${selectedPass.metric}`;
   function callPaymentGateway() {
     if (selectedPass && selectedLocation && selectedQuantity) {
-      // setLoader(true);
-      makePayment();
+      setLoader(true);
+      makePayment(bookingData, setLoader, setModal);
     } else {
       console.log("Please ensure that name, email and the service is selected");
     }
-  }
-
-  const initializeRazorpay = () => {
-    return new Promise((resolve) => {
-      const script = document.createElement("script");
-      script.src = "https://checkout.razorpay.com/v1/checkout.js";
-
-      script.onload = () => {
-        resolve(true);
-      };
-      script.onerror = () => {
-        resolve(false);
-      };
-      document.body.appendChild(script);
-    });
-  };
-  async function makePayment() {
-    const res = await initializeRazorpay();
-    if (!res) {
-      alert("Razorpay SDK Failed to load");
-      return;
-    }
-    createOrder("9767137428", bookingData.id, 2, function (data) {
-      const newData = {
-        orderId: "data",
-      };
-      makePaymentCall(newData);
-    });
-  }
-
-  async function makePaymentCall(data) {
-    var config = {
-      method: "post",
-      url: `https://us-central1-coworkingspaces-48082.cloudfunctions.net/app`,
-      headers: {
-        ContentType: "application/json",
-      },
-      data: data,
-    };
-    await axios(config)
-      .then(function (response) {
-        completeTransaction(response.data);
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
-  }
-  function completeTransaction(responseData) {
-    setLoader(false);
-    var options = {
-      key: process.env.NEXT_PUBLIC_RZP_ID, // Enter the Key ID generated from the Dashboard
-      name: "GrowthSchool",
-      currency: responseData.currency,
-      amount: responseData.amount,
-      order_id: responseData.id,
-      prefill: {},
-      handler: function (response) {
-        // Validate payment at server - using webhooks is a better idea.
-        if (response.razorpay_payment_id) {
-          setModal(true);
-        }
-      },
-    };
-    const paymentObject = new window.Razorpay(options);
-    paymentObject.open();
   }
 
   return (
